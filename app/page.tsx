8f440659e5000816e080e71b918d99dcb849228d
Maria <ev.maria.pro@gmail.com>
2025-06-18 13:34:37 -0400

    fix mobile version and font boldness to adapt to a new font

--- app/page.tsx
+++ app/page.tsx
@@ -14,13 +14,13 @@ export default function HomePage() {
       <main className="flex flex-col items-center justify-center min-h-screen">
         <div className="bg-stone-200 w-full h-screen flex flex-col sm:flex-row items-center justify-between p-4 sm:px-8 lg:px-16 xl:px-32">
             <div className="flex flex-col sm:items-start items-center justify-center p-8">
-              <h1 className="text-4xl font-bold mb-4">Let&#39;s make career change easier, together.</h1>
-              <p className="text-lg text-gray-600 mb-8">
+              <h1 className="text-4xl md:text-6xl mb-6">Let&#39;s make career change easier, together.</h1>
+              <p className="text-xl text-gray-600 mb-8">
                 Join open projects, find volunteering network, chat with fellow career changers
               </p>
                 <div className="flex gap-4 mt-4">
                 <Button asChild>
-                  <Link href="/register"> Join our community <span aria-hidden="true" className="font-bold">→</span></Link>
+                  <Link href="/register"> Join our community <span aria-hidden="true">→</span></Link>
                 </Button>
                 <Button variant="secondary">
                   <Link href="/about">More about Kite</Link>
@@ -35,8 +35,8 @@ export default function HomePage() {
           
         </div>
         <div className="bg-white flex flex-col items-center justify-between py-12 px-2 sm:px-0">
-          <h2 className="text-3xl font-bold mt-4">How can we help you</h2>
-          <p className="p-2 mb-4">Your next step toward brighter days!</p>
+          <h2 className="text-3xl md:text-5xl mt-4">How can we help you</h2>
+          <p className="text-xl p-2 mb-4">Your next step toward brighter days!</p>
             <div className="flex flex-row p-4 overflow-x-auto sm:px-0 flex-wrap sm:justify-center">
               <ServiceCard 
                 title="Join our community"
