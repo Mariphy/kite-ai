8cef27d77744eed565ef4314341380a4df5cf22c
Maria <ev.maria.pro@gmail.com>
2025-06-23 21:41:29 -0400

    add book cards

--- app/page.tsx
+++ app/page.tsx
@@ -60,7 +60,7 @@ export default function HomePage() {
                 description="Browse free resources that can help you in your transition."
                 buttonTitle="Start browsing"
                 icon={<Image src="/images/icon3.svg" alt="Care Icon" width={60} height={60} />}
-                buttonLink="/"
+                buttonLink="/resources"
               />
             </div>
         </div>
@@ -94,7 +94,32 @@ export default function HomePage() {
         <div className="bg-white flex flex-col items-center text-center justify-between py-12 px-2 sm:px-0">
           <h2 className="text-3xl md:text-5xl mt-4">Curated Books</h2>
           <p className="text-xl p-2 mb-4">Not sure where to start? <br/>Check the books that helped other career changers most.</p>
-
+          <div className="flex flex-row p-4 overflow-x-auto sm:px-0 flex-wrap sm:justify-center">
+            <StoryCard 
+              image={<Image src="/images/nsa.svg" alt="Never Search Alone book cover" width={320} height={180} className="rounded-t-lg"/>}
+              title="Never Search Alone"
+              description="Phyl Terry"
+              url="https://www.phyl.org/"
+            />
+            <StoryCard 
+              image={<Image src="/images/job-moves.svg" alt="Job Moves book cover" width={320} height={180} className="rounded-t-lg"/>}
+              title="Job Moves"
+              description="Ethan Bernstein, Michael B. Horn, Bob Moesta"
+              url=""
+            />
+            <StoryCard 
+              image={<Image src="/images/designing.svg" alt="Designing Your Life book cover" width={320} height={180} className="rounded-t-lg"/>}
+              title="Designing Your Life"
+              description="Bill Burnett, Dave Evans"
+              url="https://www.designingyour.life/"
+            />
+            <StoryCard 
+              image={<Image src="/images/two-hour.svg" alt="Two Hour Job Search book cover" width={320} height={180} className="rounded-t-lg"/>}
+              title="The Two-Hour Job Search"
+              description="Steve Dalton"
+              url="https://2hourjobsearch.com/"
+            />
+          </div>  
         </div>
       </main>
     </>
